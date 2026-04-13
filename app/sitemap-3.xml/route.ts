d4a829fe57aca016977ac77edb8e5076e2bf3ed4
Rox voices ai <simubalishawn2006@gmail.com>
2026-04-13 23:14:37 +0200

    Add files via upload

---
+++ app/sitemap-3.xml/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server'
+import { getChunk, buildUrlsetXml, XML_HEADERS } from '@/lib/sitemap-utils'
+
+export const dynamic = 'force-static'
+
+export async function GET() {
+  return new NextResponse(buildUrlsetXml(getChunk(3)), {
+    status: 200,
+    headers: XML_HEADERS,
+  })
+}
